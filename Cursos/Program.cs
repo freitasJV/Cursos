40b2beb13977b3f956a4c782cc7a7aea6b1aca3e
freitasJV <jose.freitas16@fatec.sp.gov.br>
2020-05-31 16:02:04 -0300

    Add project files.

---
+++ Cursos/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Cursos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<int> set = new HashSet<int>();
+            Console.Write("How many students for course A? ");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= n; i++)
+            {
+                Console.Write($"What id for student {i}? ");
+                int cod = int.Parse(Console.ReadLine());
+                set.Add(cod);
+            }
+
+            Console.Write("How many students for course B? ");
+            n = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= n; i++)
+            {
+                Console.Write($"What id for student {i}? ");
+                int cod = int.Parse(Console.ReadLine());
+                set.Add(cod);
+            }
+
+            Console.Write("How many students for course C? ");
+            n = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= n; i++)
+            {
+                Console.Write($"What id for student {i}? ");
+                int cod = int.Parse(Console.ReadLine());
+                set.Add(cod);
+            }
+
+            Console.WriteLine($"Total Students: {set.Count}");
+        }
+    }
+}
